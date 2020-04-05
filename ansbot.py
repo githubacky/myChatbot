d3b3e0e14d21ee1bfbe8e60e9846a7a4f6e2cbcb
githubacky <acky.c1@gmail.com>
2020-04-05 13:41:15 +0900

    Update scrape_concert.py etc.

--- ansbot.py
+++ ansbot.py
@@ -2,7 +2,7 @@ from pymongo import MongoClient
 from urllib.parse import urljoin
 from bs4 import BeautifulSoup
 from scrape_stock import main_stock
-from scrape_concert_db import main_concert
+from scrape_concert import main_concert
 
 #---- 外部データ読み込み
 ext_data = "bot_data.txt"
@@ -10,6 +10,12 @@ with open(ext_data, "r", encoding="utf-8") as f:
 	tmp = f.read()
 bot_list = tmp.splitlines()
 
+con_data = "concert_data.txt"
+with open(con_data, "r", encoding="utf-8") as f:
+	tmp = f.read()
+con_list = tmp.splitlines()
+
+#---- 読み込んだ外部データを辞書に変換
 #---- 読み込んだ外部データを辞書に変換
 bot_dict = {}
 for li in bot_list:
@@ -35,12 +41,26 @@ while True:
 	#---- Crawling
 	if "Crawling 株価" in ans:
 		while True:
-			stcnum = input("stock> 銘柄コードは？")
-			main_stock(stcnum)
+			stcnum = input(" stock> 銘柄コードは？")
 			if "bye" in stcnum:
 				break
+			main_stock(stcnum)
 
 	if "Crawling 演奏会" in ans:
-		print('おすすめの演奏会をピックアップしました。')
-		main_concert()
+		print('知りたい項目を選択してください')
+		print('\n'.join(con_list))
+		while True:
+			conc = input(" concert> ")
+			if "bye" in conc:
+				break
+			elif conc == "1":
+				main_concert(conc, 0)
+			elif conc == "2":
+				print("何月ですか？")
+				mnth = input(" concert> ")
+				print("{0}月開催の演奏会は以下の通りです".format(mnth))
+				main_concert(conc, mnth)
+			else:
+				print("現在準備中のため、別の項目を選択してください(>_<)")
+				#main_concert(conc, 0)
 
