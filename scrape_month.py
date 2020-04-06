761ff0f8dc310713f1530d161cb774308d53752c
githubacky <55225840+githubacky@users.noreply.github.com>
2020-04-06 10:08:04 +0900

    update files

--- scrape_month.py
+++ scrape_month.py
@@ -5,21 +5,10 @@ import lxml.html
 import sqlite3
 from bs4 import BeautifulSoup
 
-def main_concert(num, mnth):
-	num = int(num)
-	if num == 1:
-		url = 'https://i-amabile.com/'
-	elif num == 2:
-		url = "https://i-amabile.com/archives/2020-" + str(mnth).zfill(2)
-	else:
-		url = 'https://i-amabile.com/'
+def main_concert_month():
+	url = 'https://i-amabile.com/2020-07'
 	html = fetch(url)
-	if num == 1:
-		books = scrape_pickup(html, url)
-	elif num == 2:
-		books = scrape_month(html, url)
-	else:
-		books = scrape_pickup(html, url)
+	books = scrape(html, url)
 	#save('concert.csv', books)
 	save_sqlite3('concert.db', books)
 
@@ -27,13 +16,18 @@ def fetch(url: str) -> str:
 	r = requests.get(url)
 	return r.text
 
-def scrape_pickup(html: str, base_url: str) -> List[dict]:
+def scrape(html: str, base_url: str) -> List[dict]:
 	books =[]
 	html = lxml.html.fromstring(html)
 	html.make_links_absolute(base_url)
 
 	for idx in range(2, 12, 2):
-		# cssel = '#content > div > div:nth-child(3) > table:nth-child(' + str(idx) + ') > tbody' # > tr > td:nth-child(1)' # > a'
+		# #content > div > a:nth-child(7)
+		# #content > div > p:nth-child(5)
+		# #content > div > b:nth-child(6) > a
+		# #content > div > b:nth-child(9) > a
+		# #content > div > p:nth-child(12)
+		# #content > div > b:nth-child(13) > a
 		cssel = '#content > div > div:nth-child(3) > table:nth-child(' + str(idx) + ') > tr > td:nth-child(1) > a'
 		a = html.cssselect(cssel)
 		url = a[0].get('href')
@@ -43,28 +37,6 @@ def scrape_pickup(html: str, base_url: str) -> List[dict]:
 		#print(url, title)
 
 	return books
-
-def scrape_month(html: str, base_url: str) -> List[dict]:
-	books =[]
-
-	response = requests.get(base_url)
-	response.encoding = response.apparent_encoding
-
-	bs = BeautifulSoup(response.text, 'html.parser')
-
-	cname = bs.select("#content > div > b > a")
-
-	cnt = 1
-	for cn in cname:
-		flag = cn.getText()[0]
-		if flag == "●":
-			title = cn.getText().replace("●","")
-			url = cn.get('href')
-			#print(str(cnt).zfill(2) + ". " + title, url)
-			cnt += 1
-			books.append({'url':url, 'title':title})
-	return books
-
 
 def save(file_path: str, books: List[dict]):
 	with open(file_path, 'w', newline='') as f:
@@ -90,4 +62,4 @@ def save_sqlite3(file_path: str, books: List[dict]):
 	conn.close()
 
 if __name__ == '__main__':
-	main_concert()
+	main_concert_month()
